67107ba477e3bd36ed3f6864cee17d9492365d46
pbalkonskiy <17400041@study.bsuir.by>
2023-02-06 21:21:05 +0300

    Added creation & elimination signals for Cart app.

---
+++ cart/signals.py
@@ -0,0 +1,25 @@
+from django.db.models import signals
+from django.dispatch import receiver
+
+from users.models import Customer
+from cart.models import Cart
+
+
+@receiver(signals.post_save, sender=Customer)
+def create_customer_cart(sender, instance, created, **kwargs):
+    """
+    Creates a new record in the Customer table creates a new Cart
+    with an id (pk) field corresponding to the id (pk) of the new Customer.
+    """
+    if created:
+        cart = Cart.objects.create(id=instance.id, status="OK")
+        instance.cart = cart
+        instance.save()
+
+
+@receiver(signals.post_delete, sender=Customer)
+def delete_customer_cart(sender, instance, **kwargs):
+    """
+    Deletes the Cart corresponding to the Customer.
+    """
+    Cart.objects.get(id=instance.id).delete()

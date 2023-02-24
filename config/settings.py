10075a7dc7bcf7999dc9f4182487cf4016c03fb9
pbalkonskiy <17400041@study.bsuir.by>
2023-02-24 16:28:28 +0300

    Added Celery & Redis services to the Docker Compose. Refactored requirements.txt.

--- config/settings.py
+++ config/settings.py
@@ -187,3 +187,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Celery & Redis
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
